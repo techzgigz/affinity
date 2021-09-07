cf1f78e3f55e4f25e6a4c14106aa4c7e36b3a3e6
Vikash Sharma <1672153@tcs.com>
2021-09-07 17:17:09 +0000

    code

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dbConfig = require("./app/config/db.config");
-const AffinidiWallet = require('@affinidi/wallet-node-sdk') 
+// const AffinidiWallet = require('@affinidi/wallet-node-sdk') 
 const app = express();
-
+const crypto = require("crypto");
+const fs = require('fs');
 var corsOptions = {
   origin: "http://localhost:5000"
 };
@@ -36,36 +37,48 @@ db.mongoose
 
 // simple route
 app.get("/", async (req, res) => {
-  const options1 = {
-    env: 'prod',
-    apiKey: 'd22ce62c-3cfe-436b-ac80-6bdcfebdaf43' , //ee1accf234b414ca30f343d731102015860d23be8877e42f70775a469f7f0da6
-    didMethod: 'elem' // 'elem' (default) or 'jolo'
-  }
-  // "@affinidi/wallet-node-sdk": "^6.0.0-beta.10",
-  //const { did, encryptedSeed } = await AffinidiWallet.createWallet().createWallet(options1,"rohits")
-  //console.log(did)
 
-// const keyParams = { encryptedSeed, password }
-// const email = 'example@affinity-project.org'
-// const userPassword = 'Password123'
-// const options = { env: 'dev' }
-// const messageParameters = { message: 'Welcome to Affinity, here is your OTP: {{CODE}}' } //  (optional)
-
-// const token = await AffinidiWallet.createWallet().initiateSignUpByEmail(options, email, userPassword, messageParameters)
-// const confirmationCode = '123456'
-//const wallet = await AffinidiWallet.createWallet().completeSignUp(options, token, confirmationCode, keyParams)
-
-const username = 'great_user'
-const password = 'Password123'
-const optionss = { env: 'dev' }
-
-//const wallet = await AffinidiWallet.createWallet().signUpWithUsername(options1, username, password)
-//const wallet = await AffinidiWallet.createWallet().logInWithPassword(options1, username, password)
-
-//console.log(wallet.encryptedSeed)
-  
   res.json({ message: "Welcome to Affinity application." });
 });
+app.get("/key", async (req, res) => {
+
+  crypto.generateKeyPair('rsa', {
+    modulusLength: 4096,    // options
+    publicExponent: 0x10101,
+    publicKeyEncoding: {
+      type: 'pkcs1',
+      format: 'pem'
+    },
+    privateKeyEncoding: {
+      type: 'pkcs8',
+      format: 'pem',
+      cipher: 'aes-192-cbc',
+      passphrase: 'affinity'
+    }
+  }, (err, publicKey, privateKey) => { // Callback function
+    if (!err) {
+      // Prints new asymmetric key pair
+      console.log("Public Key is : ", publicKey);
+      console.log();
+      console.log("Private Key is: ", privateKey);
+
+      fs.writeFileSync("public.pem", Buffer.from(publicKey));
+      fs.writeFileSync("private.pem", Buffer.from(privateKey));
+    }
+    else {
+      // Prints error
+      console.log("Errr is: ", err);
+    }
+
+  });
+
+  res.json({ message: "Welcome to Affinity application." });
+});
+
+
+// console.log(publicKey,privateKey)
+
+
 require("./app/routes/auth.routes")(app);
 require("./app/routes/wallet.routes")(app);
 
